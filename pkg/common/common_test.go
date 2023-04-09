e5199be2f9c6077114ee0934cbfbbd7476814156
belenkom <belenkom>
2023-04-09 19:06:28 +0300

    Correct test

--- pkg/common/common_test.go
+++ pkg/common/common_test.go
@@ -2,16 +2,14 @@ package common
 
 import (
 	"testing"
-	"sync"
 )
 
 func TestCalculateWeight(t *testing.T) {
-    keyboard := map[byte][2]int{
+	keyboard := map[byte][2]int{
 		'q': {0, 0}, 'w': {0, 1}, 'e': {0, 2}, 'r': {0, 3}, 't': {0, 4}, 'y': {0, 5}, 'u': {0, 6}, 'i': {0, 7}, 'o': {0, 8}, 'p': {0, 9},
 		'a': {1, 0}, 's': {1, 1}, 'd': {1, 2}, 'f': {1, 3}, 'g': {1, 4}, 'h': {1, 5}, 'j': {1, 6}, 'k': {1, 7}, 'l': {1, 8},
 		'z': {2, 0}, 'x': {2, 1}, 'c': {2, 2}, 'v': {2, 3}, 'b': {2, 4}, 'n': {2, 5}, 'm': {2, 6},
 	}
-	keyboardMutex := sync.RWMutex{}
 
 	testCases := []struct {
 		name     string
@@ -27,7 +25,7 @@ func TestCalculateWeight(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			result := CalculateWeight(tc.input, &keyboard, &keyboardMutex)
+			result := CalculateWeight(tc.input, keyboard)
 			if result != tc.expected {
 				t.Errorf("Expected %d, got %d", tc.expected, result)
 			}
